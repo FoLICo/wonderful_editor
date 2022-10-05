d14a123b01d99fe51da602e807a9200e9c97885a
Hori Kouta <kouta2880@gmail.com>
2022-10-06 08:08:43 +0900

    各model に validation を追加した際のエラーを訂正しました。

--- app/models/article.rb
+++ app/models/article.rb
@@ -18,8 +18,8 @@
 #  fk_rails_...  (user_id => users.id)
 #
 class Article < ApplicationRecord
-  validates :body, presence:true
-  validates :title, presence:true
+  validates :body, presence: true
+  validates :title, presence: true
 
   belongs_to :user
   has_many :comments, dependent: :destroy
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -20,7 +20,7 @@
 #  fk_rails_...  (user_id => users.id)
 #
 class Comment < ApplicationRecord
-  validates :body, presence:true
+  validates :body, presence: true
 
   belongs_to :user
   belongs_to :article
