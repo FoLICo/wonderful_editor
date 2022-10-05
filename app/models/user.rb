7c066596a76b340e93ffb6376b17398e6d6cac55
Hori Kouta <kouta2880@gmail.com>
2022-10-05 15:58:24 +0900

    作成済みのER図に基づいて各種テーブルとmodelを作成した際のエラーを処理しました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
 
-  has_many :articles
-  has_many :comments
-  has_many :article_likes
-
+  has_many :articles, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :article_likes, dependent: :destroy
 end
