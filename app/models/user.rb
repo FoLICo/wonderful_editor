fb1a2a0e0dec6e356b8a0ea4c678bf620d4eabdf
Hori Kouta <kouta2880@gmail.com>
2022-10-07 19:11:58 +0900

    validationのレイアウトを変更しました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,6 +29,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   validates :name, presence: true, uniqueness: true
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
