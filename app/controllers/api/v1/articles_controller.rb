ac6585351b69172dc8bf5a505312dd29c93ef878
Hori Kouta <kouta2880@gmail.com>
2022-10-12 21:50:34 +0900

    記事作成のAPIを実装しました。 同時にテストも作成しました。

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module V1
     class ArticlesController < BaseApiController
+      # before_action :current_user
+
       def index
         articles = Article.all.order(updated_at: :desc)
         # 下記でserializerを使って出力できるようにする！
@@ -11,7 +13,23 @@ module Api
         # binding.pry
         article = Article.find(params[:id])
         # 下記でserializerを使って出力できるようにする！
-        render json: article, serializer: Api::V1::ArticleSerializer  # 出力するデータが配列ではない場合はeach_serializerではなく、serializerを設定
+        render json: article, serializer: Api::V1::ArticleSerializer # 出力するデータが配列ではない場合はeach_serializerではなく、serializerを設定
+      end
+
+      def create
+        # binding.pry
+        # article = current_user.articles.new(article_params)
+        article = Article.new(article_params)
+        # binding.pry
+        article.save!
+        render json: article, serializer: Api::V1::ArticleSerializer
+      end
+
+      def article_params
+        # binding.pry
+        # params.require(:article).permit(:title, :body)  #.merge(user_id: current_user.id)
+        params.require(:article).permit(:title, :body).merge(user_id: current_user.id)
+        # binding.pry  #ここのpryは禁物
       end
     end
   end
--- app/controllers/api/v1/base_api_controller.rb
+++ app/controllers/api/v1/base_api_controller.rb
@@ -1,2 +1,7 @@
 class Api::V1::BaseApiController < ApplicationController
+  def current_user
+    @current_user ||= User.first
+    # ||の左側がtrueだったら右側は実行しない(if => false)
+    # ||の左側がfalse/nilだったら右側を実行する（if => true）
+  end
 end
